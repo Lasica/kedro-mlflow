b1d68f087fef1e9d51536d152e9d265a9b8d55b0
Calychas <kacper.lesniara@gmail.com>
2025-04-10 16:53:27 +0200

    fix: close all opened runs, not just one

--- kedro_mlflow/framework/hooks/mlflow_hook.py
+++ kedro_mlflow/framework/hooks/mlflow_hook.py
@@ -444,7 +444,9 @@ class MlflowHook:
                     f"The run '{mlflow.active_run().info.run_id}' was already opened before launching 'kedro run' so it is not closed. You should close it manually."
                 )
             else:
-                mlflow.end_run()
+                # we opened a run for each node, so we must close them all
+                while mlflow.active_run() is not None:
+                    mlflow.end_run()
 
         else:
             switch_catalog_logging(catalog, True)
